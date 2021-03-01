3e3ab8487dd0a8935f4749ef6577057b851a4eef
Rick van der Wal <r.vanderwal@student.science.ru.nl>
2021-03-01 19:16:51 +0100

    Greatly improved char_iterator.rs

--- src/char_iterator.rs
+++ src/char_iterator.rs
@@ -1,16 +1,10 @@
-use std::iter::{Enumerate, FlatMap, Map, Repeat, Zip};
 use std::str::Chars;
 
-pub type CharIterator<'a> = FlatMap<Enumerate<TerminatedLines<'a>>, Map<Zip<Enumerate<Chars<'a>>, Repeat<usize>>, Reorder>, Position>;
-type Position = fn((usize, &str)) -> Map<Zip<Enumerate<Chars>, Repeat<usize>>, Reorder>;
-type Reorder = fn(((usize, char), usize)) -> ((usize, usize), char);
-
-fn position((row, l): (usize, &str)) -> Map<Zip<Enumerate<Chars>, Repeat<usize>>, fn(((usize, char), usize)) -> ((usize, usize), char)> {
-    l.chars().enumerate().zip(std::iter::repeat(row)).map(reorder)
-}
-
-fn reorder(((col, c), row): ((usize, char), usize)) -> ((usize, usize), char) {
-    ((row, col), c)
+#[derive(Clone, Debug)]
+pub struct CharIterator<'a> {
+    chars: Chars<'a>,
+    row: usize,
+    col: usize
 }
 
 pub trait CharIterable<'a> {
@@ -19,36 +13,28 @@ pub trait CharIterable<'a> {
 
 impl<'a> CharIterable<'a> for &'a str {
     fn iter_char(&self) -> CharIterator<'a> {
-        self.lines_terminated().enumerate().flat_map(position as Position)
+        CharIterator {
+            chars: self.chars(),
+            row: 1,
+            col: 1
+        }
     }
 }
 
-trait LineTerminatable<'a> {
-    fn lines_terminated(self) -> TerminatedLines<'a>;
-}
-
-impl<'a> LineTerminatable<'a> for &'a str {
-    fn lines_terminated(self) -> TerminatedLines<'a> {
-        TerminatedLines { input: self }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct TerminatedLines<'a> {
-    input: &'a str,
-}
-
-impl<'a> Iterator for TerminatedLines<'a> {
-    type Item = &'a str;
+impl<'a> Iterator for CharIterator<'a> {
+    type Item = ((usize, usize), char);
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.input.is_empty() {
-            None
-        } else {
-            let split = self.input.find('\n').map(|i| i + 1).unwrap_or(self.input.len());
-            let (line, rest) = self.input.split_at(split);
-            self.input = rest;
-            Some(line)
+        let next = self.chars.next()?;
+
+        match next {
+            '\n' => {
+                self.row += 1;
+                self.col = 1;
+            }
+            _ => self.col += 1
         }
+
+        Some(((self.row, self.col), next))
     }
 }
