eb3dac624099aff4dad0c94c2a72775b6d6e09bb
Rick van der Wal <r.vanderwal@student.science.ru.nl>
2021-03-14 17:53:26 +0100

    Extracted compile errors into module

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use std::error::Error;
 use std::fmt::Debug;
 use std::fs;
 
-use crate::lexer::{error::LexError, Lexable};
-use crate::parser::ParseError;
+use error::Result;
+
+use crate::error::CompileError;
+use crate::lexer::Lexable;
 use crate::parser::SPL;
 
 mod char_iterator;
@@ -30,40 +32,49 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-type Result<T, E = CompileError> = std::result::Result<T, E>;
+mod error {
+    use std::error::Error;
+    use std::fmt;
+    use std::fmt::Debug;
 
-enum CompileError {
-    LexError(Vec<LexError>),
-    ParseError(ParseError),
-    InsufficientArguments,
-}
+    use crate::lexer::error::LexError;
+    use crate::parser::ParseError;
 
-impl fmt::Display for CompileError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            CompileError::LexError(e) => write!(f, "Lexer error:\n{}", e.iter().map(|e| format!("{}", e)).collect::<Vec<String>>().join("\n")),
-            CompileError::ParseError(e) => write!(f, "Parse error:\n{}", e),
-            CompileError::InsufficientArguments => write!(f, "Not enough arguments")
+    pub type Result<T, E = CompileError> = std::result::Result<T, E>;
+
+    pub enum CompileError {
+        LexError(Vec<LexError>),
+        ParseError(ParseError),
+        InsufficientArguments,
+    }
+
+    impl fmt::Display for CompileError {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                CompileError::LexError(e) => write!(f, "Lexer error:\n{}", e.iter().map(|e| format!("{}", e)).collect::<Vec<String>>().join("\n")),
+                CompileError::ParseError(e) => write!(f, "Parse error:\n{}", e),
+                CompileError::InsufficientArguments => write!(f, "Not enough arguments")
+            }
         }
     }
-}
 
-impl Debug for CompileError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self)
+    impl Debug for CompileError {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "{}", self)
+        }
     }
-}
 
-impl From<Vec<LexError>> for CompileError {
-    fn from(e: Vec<LexError>) -> Self {
-        CompileError::LexError(e)
+    impl From<Vec<LexError>> for CompileError {
+        fn from(e: Vec<LexError>) -> Self {
+            CompileError::LexError(e)
+        }
     }
-}
 
-impl From<ParseError> for CompileError {
-    fn from(e: ParseError) -> Self {
-        CompileError::ParseError(e)
+    impl From<ParseError> for CompileError {
+        fn from(e: ParseError) -> Self {
+            CompileError::ParseError(e)
+        }
     }
-}
 
-impl Error for CompileError {}
+    impl Error for CompileError {}
+}
