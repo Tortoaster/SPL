aed10e4600523ec1ca8a651eeb9ee970e342a3d1
Rick van der Wal <r.vanderwal@student.science.ru.nl>
2021-03-15 10:55:42 +0100

    Updated tests

--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 use spl::parser::SPL;
+use spl::lexer::Lexable;
 
 const RES_DIR: &str = "tests/res/";
 
@@ -10,7 +11,8 @@ fn parse() {
         for file in dir {
             if let Ok(file) = file {
                 let code = fs::read_to_string(file.path()).expect(format!("File {:?} is inaccessible", file.file_name()).as_str());
-                SPL::new(code.as_str()).expect(format!("Error parsing {:?}", file.file_name()).as_str());
+                let lexer = code.as_str().tokenize().expect("Failed to tokenize");
+                SPL::new(lexer.peekable()).expect(format!("Error parsing {:?}", file.file_name()).as_str());
             }
         }
     }
@@ -22,9 +24,11 @@ fn pretty_print() {
         for file in dir {
             if let Ok(file) = file {
                 let code = fs::read_to_string(file.path()).expect(format!("File {:?} is inaccessible", file.file_name()).as_str());
-                if let Ok(ast) = SPL::new(code.as_str()) {
+                let lexer = code.as_str().tokenize().expect("Failed to tokenize");
+                if let Ok(ast) = SPL::new(lexer.peekable()) {
                     let pretty = format!("{}", ast);
-                    let pretty_ast = SPL::new(pretty.as_str()).expect(format!("Error parsing prettified {:?}", file.file_name()).as_str());
+                    let pretty_lexer = pretty.as_str().tokenize().expect("Failed to tokenize");
+                    let pretty_ast = SPL::new(pretty_lexer.peekable()).expect(format!("Error parsing prettified {:?}", file.file_name()).as_str());
                     assert_eq!(ast, pretty_ast);
                 }
             }
